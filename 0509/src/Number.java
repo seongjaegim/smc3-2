e572139ee2eee6903e496ce5a9ef5de60395fa99
Administrator <Administrator@501-S17>
2022-05-09 10:39:28 +0900

    sdg

---
+++ 0509/src/Number.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Number {
+	public static void main(String[] args) {
+		System.out.println("10개의 랜덤 숫자 중 하나를 맞춰보세요(1~50까지");
+
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+
+		int[] arr = new int[10];
+		int diff = 0;
+		for (int i = 0; i < 10; i++) {
+			arr[i] = (int) (Math.random() * 50 + 1);
+			System.out.println(arr[i] + " ");
+		}
+		System.out.println();
+		for (int i = 0; i < 10; i++) {
+			if (arr[i] == num) {
+				System.out.println("당첨! 일치하는 숫자가 아닙니다.");
+				diff = 1;
+			}
+		}
+		if (diff == 0)
+			System.out.println("꽝! 다음 기회에~");
+	}
+}
---
+++ 0509/src/Sum.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int a, b = 0;
+		int sum = 0;
+		System.out.print("시작 숫자: ");
+		a = scan.nextInt();
+		System.out.println("마지막 숫자: ");
+		b = scan.nextInt();
+
+		for (int i = a; i <= b; i++) {
+			sum += i;
+		}
+		System.out.println(a + "부터 " + b + "까지의 합은 " + sum);
+	}
+
+}
